4deb58b551cbbccabd06c3c53943bf01eaf2a2ef
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-07-31 01:43:00 +0530

    Reafactored: Method For Check Sample Emails Are Valid Or Invalid

--- src/com/bridgelab/UserRegistration.java
+++ src/com/bridgelab/UserRegistration.java
@@ -51,9 +51,9 @@ public class UserRegistration {
 		isEmail = matcherObject.matches();
 
 		if (isEmail)
-			System.out.println(email + " is a Valid Email address\n");
+			System.out.println(email + " -> is a Valid Email address\n");
 		else
-			System.out.println(email + " is an Invalid Email address");
+			System.out.println(email + " -> is an Invalid Email address");
 	}
 
 	public static void checkValidMobileNumber(String mobileNumber) {
@@ -76,7 +76,8 @@ public class UserRegistration {
 	public static void checkValidPassword(String password) {
 
 		boolean isPassword;
-		String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()])([a-zA-Z0-9]*).{8,}$"; //At least one Special Character 
+		String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()])([a-zA-Z0-9]*).{8,}$"; // At least one Special
+																									// Character
 		Pattern patternObject = Pattern.compile(passwordRegex);
 		if (password == null) {
 			isPassword = false;
@@ -89,8 +90,6 @@ public class UserRegistration {
 		else
 			System.out.println(password + " is an Invalid Password");
 	}
-	
-	
 
 	public static void main(String[] args) {
 
@@ -119,6 +118,83 @@ public class UserRegistration {
 		checkValidPassword(password);
 
 		scannerObject.close();
+		System.out.println("---------------------------------------------------");
+
+		System.out.println("\n         Valid Emails\n");
+
+		email = "abc@yahoo.com";
+		checkValidEmail(email);
+
+		email = "abc-100@yahoo.com";
+		checkValidEmail(email);
+
+		email = "abc.100@yahoo.com";
+		checkValidEmail(email);
+
+		email = "abc111@abc.com";
+		checkValidEmail(email);
+
+		email = "abc-100@abc.net";
+		checkValidEmail(email);
+
+		email = "abc.100@abc.com.au";
+		checkValidEmail(email);
+
+		email = "abc@1.com";
+		checkValidEmail(email);
+		
+		email = "abc@gmail.com.com";
+		checkValidEmail(email);
+		
+		email = "abc+100@gmail.com";
+		checkValidEmail(email);
+		
+		
+
+		System.out.println("---------------------------------------------------");
+
+		System.out.println("\n         Invalid Emails\n");
+
+		email = "abc";
+		checkValidEmail(email);
+
+		email = "abc@.com.my";
+		checkValidEmail(email);
+
+		email = "abc123@gmail.a";
+		checkValidEmail(email);
+
+		email = "abc123@.com";
+		checkValidEmail(email);
+
+		email = "abc123@.com.com";
+		checkValidEmail(email);
+
+		email = ".abc@abc.com";
+		checkValidEmail(email);
+
+		email = "abc()*@gmail.com";
+		checkValidEmail(email);
+
+		email = "abc@%*.com";
+		checkValidEmail(email);
+
+		email = "abc..2002@gmail.com";
+		checkValidEmail(email);
+
+		email = "abc.@gmail.com";
+		checkValidEmail(email);
+
+		email = "abc@abc@gmail.com";
+		checkValidEmail(email);
+
+		email = "abc@gmail.com.1a";
+		checkValidEmail(email);
+
+		email = "abc@gmail.com.aa.au";
+		checkValidEmail(email);
+
+		System.out.println("---------------------------------------------------");
 
 	}
 
