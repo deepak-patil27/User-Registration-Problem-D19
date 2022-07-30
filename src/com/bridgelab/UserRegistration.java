6cd87ba895e61284cf63cc5f4b49615a01485757
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-07-30 19:19:15 +0530

    Added: User Need To Enter A Valid Email

--- src/com/bridgelab/UserRegistration.java
+++ src/com/bridgelab/UserRegistration.java
@@ -14,15 +14,14 @@ public class UserRegistration {
 			isFirstName = false;
 		}
 		Matcher matcherObject = patternObject.matcher(firstName);
-		isFirstName =  matcherObject.matches();
+		isFirstName = matcherObject.matches();
 
-		if(isFirstName)
-			System.out.println(firstName+" is an Valid First Name");
+		if (isFirstName)
+			System.out.println(firstName + " is an Valid First Name");
 		else
-			System.err.println(firstName+" is an Invalid First Name");
+			System.err.println(firstName + " is an Invalid First Name");
 	}
-	
-	
+
 	public static void checkValidLastName(String lastName) {
 
 		boolean isLastName;
@@ -39,7 +38,23 @@ public class UserRegistration {
 		else
 			System.err.println(lastName + " is an Invalid Last Name");
 	}
-	
+
+	public static void checkValidEmail(String email) {
+
+		boolean isEmail;
+		String emailRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
+		Pattern patternObject = Pattern.compile(emailRegex);
+		if (email == null) {
+			isEmail = false;
+		}
+		Matcher matcherObject = patternObject.matcher(email);
+		isEmail = matcherObject.matches();
+
+		if (isEmail)
+			System.out.println(email + " is a Valid Email address\n");
+		else
+			System.out.println(email + " is an Invalid Email address");
+	}
 
 	public static void main(String[] args) {
 
@@ -50,16 +65,17 @@ public class UserRegistration {
 		System.out.println("Enter Your First Name");
 		String firstName = scannerObject.next();
 		checkValidFirstName(firstName);
-		
+
 		System.out.println("Enter Your Last Name");
 		String lastName = scannerObject.next();
 		checkValidLastName(lastName);
 
+		System.out.println("Enter Your Email Address");
+		String email = scannerObject.next();
+		checkValidEmail(email);
 
 		scannerObject.close();
 
 	}
-
-
 
 }
