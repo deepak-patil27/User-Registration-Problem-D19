67895c9f6ba9b7b82925eda342fc712f548abb68
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-07-30 20:14:46 +0530

    Added: User Need To Enter A Valid Mobile Number As Pre-Defined

--- src/com/bridgelab/UserRegistration.java
+++ src/com/bridgelab/UserRegistration.java
@@ -8,7 +8,7 @@ public class UserRegistration {
 	public static void checkValidFirstName(String firstName) {
 
 		boolean isFirstName;
-		String firstNameRegex = "[A-Z]{1}[a-z]{2,}";
+		String firstNameRegex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern patternObject = Pattern.compile(firstNameRegex);
 		if (firstName == null) {
 			isFirstName = false;
@@ -17,7 +17,7 @@ public class UserRegistration {
 		isFirstName = matcherObject.matches();
 
 		if (isFirstName)
-			System.out.println(firstName + " is an Valid First Name");
+			System.out.println(firstName + " is an Valid First Name\n");
 		else
 			System.err.println(firstName + " is an Invalid First Name");
 	}
@@ -25,7 +25,7 @@ public class UserRegistration {
 	public static void checkValidLastName(String lastName) {
 
 		boolean isLastName;
-		String lastNameRegex = "[A-Z]{1}[a-z]{2,}";
+		String lastNameRegex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern patternObject = Pattern.compile(lastNameRegex);
 		if (lastName == null) {
 			isLastName = false;
@@ -34,7 +34,7 @@ public class UserRegistration {
 		isLastName = matcherObject.matches();
 
 		if (isLastName)
-			System.out.println(lastName + " is an Valid Last Name");
+			System.out.println(lastName + " is an Valid Last Name\n");
 		else
 			System.err.println(lastName + " is an Invalid Last Name");
 	}
@@ -56,23 +56,44 @@ public class UserRegistration {
 			System.out.println(email + " is an Invalid Email address");
 	}
 
+	public static void checkValidMobileNumber(String mobileNumber) {
+
+		boolean isMobileNumber;
+		String mobileRegex = "^[0-9]{2}\\s{1}[0-9]{10}$";
+		Pattern patternObject = Pattern.compile(mobileRegex);
+		if (mobileNumber == null) {
+			isMobileNumber = false;
+		}
+		Matcher matcherObject = patternObject.matcher(mobileNumber);
+		isMobileNumber = matcherObject.matches();
+
+		if (isMobileNumber)
+			System.out.println(mobileNumber + " is a Valid  Mobile Number\n");
+		else
+			System.out.println(mobileNumber + " is an Invalid Mobile Number");
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("           Welcome To User Registration");
 
 		Scanner scannerObject = new Scanner(System.in);
 
-		System.out.println("Enter Your First Name");
-		String firstName = scannerObject.next();
+		System.out.println("Enter Your First Name:");
+		String firstName = scannerObject.nextLine();
 		checkValidFirstName(firstName);
 
-		System.out.println("Enter Your Last Name");
-		String lastName = scannerObject.next();
+		System.out.println("Enter Your Last Name:");
+		String lastName = scannerObject.nextLine();
 		checkValidLastName(lastName);
 
-		System.out.println("Enter Your Email Address");
-		String email = scannerObject.next();
+		System.out.println("Enter Your Email Address:");
+		String email = scannerObject.nextLine();
 		checkValidEmail(email);
+
+		System.out.println("Enter Your Mobile Number:");
+		String mobileNumber = scannerObject.nextLine();
+		checkValidMobileNumber(mobileNumber);
 
 		scannerObject.close();
 
